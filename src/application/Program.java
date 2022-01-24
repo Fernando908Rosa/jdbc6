7f5b4e1e4ccf5bc49a50c5cdfe3f85fe6ec99aab
fernandorosa <fernandodarosa908@gmail.com>
2022-01-24 18:06:19 -0300

    alterando projeto jdbc4

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,6 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -12,39 +10,31 @@ public class Program {
 
 	public static void main(String[] args) {
 			
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
-	Connection conn =  null;
-	PreparedStatement st = null;
-	try {
-		conn = DB.getConnection();
-		
-		st = conn.prepareStatement(
-	           "Insert iNTO seller "
-				+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-	        	+ "VALUES "
-				+ "(?, ?, ?, ?, ?)");
-	
-		st.setString(1,  "Carl Purple");	
-		st.setString(2,  "carl@mail.com");
-		st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-		st.setDouble(4, 3000.0);
-		st.setInt(5, 4);
-		
-		int rowsAffected = st.executeUpdate();
-		
-		System.out.println("Done! Rows affected: " + rowsAffected);
-		
-	}	
-	catch (SQLException e)  {
-		e.printStackTrace();
-	}
-    catch (ParseException e) {
-    	e.printStackTrace();
-    }
-    finally {
-    	DB.closeStatement(st);
-    	DB.closeConnection();
-    	}	
+	    Connection conn = null;
+	    PreparedStatement st= null;
+	    try {
+	    	conn = DB.getConnection();
+	    	
+	    	st = conn.prepareStatement(
+	    			"UPDATE seller "
+	    			+"SET BaseSalary = BaseSalary + ? "
+	    			+ "WHERE "
+	    			+ "(DepartmentId = ?)");
+	    			
+	    	st.setDouble(1, 200.0);
+	    	st.setInt(2, 2);
+	    	
+	    	int rowsAffected = st.executeUpdate();
+	    	
+	    	System.out.println("Done! Rows affected: " + rowsAffected);
+	    }	
+	    catch(SQLException e) {
+	    	e.printStackTrace();
+	    }	
+    	finally {
+    		DB.closeStatement(st);
+    		DB.closeConnection();    		
+    	}
 	}	
 }    	
     	
